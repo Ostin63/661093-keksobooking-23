5deea8bb29e6de47016ecbb79a793deb829f0e09
Dmitry <upssq@mail.ru>
2021-05-29 10:28:14 +0300

    Изменил функцию с плавающей запятой

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ getRandomInteger(2, -10);
 
 const getRandomIntlimitDecimalPlaces = (min, max, limitSigns) => {
   if (max - min >= 0) {
-    return (Math.floor(Math.random() * (max - min) + min)).toFixed(limitSigns);
+    return (Math.random() * (max - min) + min).toFixed(limitSigns);
   }
 
-  return (Math.floor(Math.random() * (min - max) + min)).toFixed(limitSigns);
+  return (Math.random() * (min - max) + min).toFixed(limitSigns);
 };
 
 getRandomIntlimitDecimalPlaces(10, 25, 4);
