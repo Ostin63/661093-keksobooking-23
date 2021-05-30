dc2cd82e46f95086e70748d7adbf082bd648d10b
Dmitry <upssq@mail.ru>
2021-05-30 09:47:09 +0300

    Преобразуйте функцию getRandomIntlimitDecimalPlaces в число

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ const getRange = (min, max) => {
   if (max >= min) {
     return Math.random() * (max - min) + min;
   }
-  return Math.random() * (min - max) + min;
+  return Math.random() * (min - max) + max;
 };
+
 const getRandomInteger = (min, max) => Math.floor(getRange(min, max));
 
 getRandomInteger(2, 10);
 
-
-const getRandomIntlimitDecimalPlaces = (min, max, limitSigns) => getRange(min, max).toFixed(limitSigns);
+const getRandomIntlimitDecimalPlaces = (min, max, limitSigns) => Number(getRange(min, max).toFixed(limitSigns));
 
 getRandomIntlimitDecimalPlaces(15, 2.5, 5);
