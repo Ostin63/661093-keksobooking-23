f78cbd60f60c403ab09f7243acd7b594c2c20c6a
Dmitry <upssq@mail.ru>
2021-06-16 19:00:16 +0300

    Добавьте сравнение для блока type

--- js/popup.js
+++ js/popup.js
@@ -2,38 +2,47 @@ import {
   getAds
 } from './data.js';
 
-const footer = document.querySelector('.footer');
+
+const footer = document.querySelector('#map-canvas');
 const similarCardTemplate = document.querySelector('#card')
   .content
   .querySelector('.popup');
 
 const getData = getAds();
 
+const cardElement = similarCardTemplate.cloneNode(true);
 
 getData.forEach((items) => {
-  const cardElement = similarCardTemplate.cloneNode(true);
-
+  const itemType  = {
+    palace: 'Дворец',
+    flat: 'Квартира',
+    house: 'Дом',
+    bungalow: 'Бунгало',
+    hotel: 'Отель',
+  };
+  cardElement.querySelector('.popup__type').textContent = itemType[items.offer.type];
   cardElement.querySelector('.popup__avatar').src = items.author.avatar;
   cardElement.querySelector('.popup__title').textContent = items.offer.title;
   cardElement.querySelector('.popup__text--address').textContent = items.offer.address;
   cardElement.querySelector('.popup__text--price').textContent = `${items.offer.price} ₽/ночь`;
-  cardElement.querySelector('.popup__type').textContent = items.offer.type;
+
   cardElement.querySelector('.popup__text--capacity').textContent = `${items.offer.rooms} комнаты для ${items.offer.guests} гостей`;
   cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${items.offer.checkin} выезд до ${items.offer.checkout}`;
-  cardElement.querySelector('.popup__description').textContent = items.offer.description;
 
-  const modifiers = items.offer.features.map((features) => `popup__features--${features}`);
+  const modifiers = items.offer.features.map((features) => `popup__feature--${features}`);
   const popupFeature = cardElement.querySelectorAll('.popup__feature');
 
   popupFeature.forEach((item) => {
-
     const modifier = item.classList[1];
 
     if (!modifiers.includes(modifier)) {
-
       item.remove();
     }
   });
+
+  cardElement.querySelector('.popup__description').textContent = items.offer.description;
+  cardElement.querySelector('.popup__photo').src = items.offer.photos;
+
 
   footer.appendChild(cardElement);
 });
